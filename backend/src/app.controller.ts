5138bf1e5f14e76a39747059480102a7ff69f4e1
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-05-27 12:16:09 +0530

    Prisma setup

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("hello")
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(":id")
+  getUser(@Param("id") id:string): string {
+    return id;
   }
 }
