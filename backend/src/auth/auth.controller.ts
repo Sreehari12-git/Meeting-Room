b712559ddf4a79c94417399bddf00f0aa88e420b
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-05-27 16:40:40 +0530

    Token generation and cookie passing

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import type { Response } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @Post("login") 
+    async login(@Body() body: LoginDto, @Res({ passthrough: true }) res: Response){
+        const result = await  this.authService.login(
+            body.email,
+            body.password
+        );
+        res.cookie("token", result.token, {
+            httpOnly: true,
+            secure: false,
+            maxAge: 10 * 24 * 60 * 60 * 1000
+        })
+
+        return result
+    }
+}
+
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService) {}
+
+    async login(email: string,password: string) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+
+        if(!user) {
+            throw new UnauthorizedException("Invalid credentials");
+        }
+
+        const match = await bcrypt.compare(password,user.password);
+
+        if(!match) {
+            throw new UnauthorizedException("Invalid credentials");
+        }
+
+        const token = jwt.sign(
+            {
+                id: user.id
+            },
+            process.env.JWT_SECRET as string, {
+                expiresIn: "10d"
+            }
+        )
+
+        return {
+            message: "Login success",
+            token
+        }
+    }
+}
+
