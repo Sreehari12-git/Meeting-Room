957168df57d3e559a1f60f165198fd1278f74077
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-05-29 14:44:58 +0530

    Created admin functionalities

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,14 +1,56 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Res } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller("users")
+@Controller("admin")
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  @Post("create")
-  async createUser(@Body() body: CreateUserDto) {
+  @Post("createUser")
+  createUser(@Body() body: CreateUserDto) {
     return this.usersService.createUser(body);
   }
+
+  @Get("getAll")
+  getAllUser() {
+    return this.usersService.getAllUsers();
+  }
+
+  @Delete("user/:email")
+  deleteUser(@Param("email") email: string) {
+    return this.usersService.deleteUser(email);
+  }
+
+  @Put("user/:email")
+  updateUser(@Param("email") email: string,@Body() body: any) {
+    return this.usersService.updateUser(
+      email,
+      body
+    );
+  }
+
+  @Post("createRooms")
+  createRoom(@Body() body: any) {
+    return this.usersService.createRooms(body);
+  }
+
+  @Get("getRooms")
+  getAllRooms() {
+    return this.usersService.getRooms();
+  }
+
+  @Delete("room/:name")
+  deleteRoom(@Param("name") name : string) {
+    return this.usersService.deleteRooms(name);
+  }
+
+  @Put("room/:name")
+  updateRoom(@Param("name") name: string,@Body() body: any) {
+    return this.usersService.updateRooms(
+      name,
+      body
+    );
+  }
 }
+
